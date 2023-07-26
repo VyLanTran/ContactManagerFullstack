0846db499be058469e73907957ebdea1c229ddb8
Vy Tran <vlt002@bucknell.edu>
2023-07-26 14:53:48 +0700

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
     return (
         <div className='bg-[#0f1a28] h-[64px] flex items-center justify-between px-7 w-full fixed top-0 z-10'>
             {/* <Link to='/'> */}
-                <h1 className='text-[#55c7d4] font-bold text-md md:text-lg lg:text-2xl'>CONNECT</h1>
-            {/* </Link> */} 
+            <h1 className='text-[#55c7d4] font-bold text-md md:text-lg lg:text-2xl'>CircleMe</h1>
+            {/* </Link> */}
 
             <div>
                 <Link to='/'><button className='text-white text-sm pr-4'>Login</button></Link>
